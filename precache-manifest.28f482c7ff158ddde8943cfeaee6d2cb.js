ead4a5cf50dfec1fe422e200c3791a5b663ca2bf
DMKuldeep <95953606+DMKuldeep@users.noreply.github.com>
2022-08-05 22:17:24 +0530

    Updates

---
+++ precache-manifest.28f482c7ff158ddde8943cfeaee6d2cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd50c88db4b9d92f8371ded748ba321",
+    "url": "/NYX/index.html"
+  },
+  {
+    "revision": "a929079668634b1b563d",
+    "url": "/NYX/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "cab26ccf510fac37b8b8",
+    "url": "/NYX/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "a929079668634b1b563d",
+    "url": "/NYX/static/js/2.2ab49a7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NYX/static/js/2.2ab49a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab26ccf510fac37b8b8",
+    "url": "/NYX/static/js/main.5a62a3a3.chunk.js"
+  },
+  {
+    "revision": "c2626463c165343e15c9",
+    "url": "/NYX/static/js/runtime-main.d93ac097.js"
+  }
+]);
